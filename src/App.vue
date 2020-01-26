c90773ae63955c7474ccb52154d93a159f1710c5
Mechi_Trybe™ <dotsolomon94@gmail.com>
2020-01-26 15:46:54 +0100

    initial update on todoapp.

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <textarea name="Enter todoapp" id="todotext" cols="30" rows="10"></textarea>
+
+    <h3 class="title">Todo</h3>
+    <div class="todos">
+       <p class="todolist"></p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import Todos from './components/DisplayTodos'
+
+
+export default {
+  name: 'app',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    Todos
+  },
+  data(){
+    return {
+      todos: [
+        {
+        id: 1,
+        title: 'todo1',
+        complete: false
+        },
+        {
+          id: 2,
+          title:'todo2',
+          complete: false
+        },
+        {
+          id: 3,
+          title: 'todo3',
+          complete: false
+        },
+        {
+          id: 4,
+          title: 'todo4',
+          complete: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/DisplayTodos.vue
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+// eslint-disable-next-line no-unused-vars
+import Todo from './todolist';
+
+export default {
+    name: 'Todos',
+    components: {
+        // eslint-disable-next-line vue/no-unused-components
+        Todo
+    }  
+}
+</script>
+
+<style scoped>
+
+</style>
